d35ee77c93934cfb7e756f5969b3b127749410ac
devansh <devansh2330@users.noreply.github.com>
2022-12-13 15:01:36 +0530

    my initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
+import 'package:mynotes/view/login_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final User = FirebaseAuth.instance.currentUser;
+              if (User?.emailVerified ?? false) {
+                print('you are a verified user');
+              } else {
+                print('you are not verified yet');
+              }
+              return const Text('Done');
+              break;
+            default:
+              return const Text('Loading...');
+          }
+        },
+      ),
+    );
+  }
+}
