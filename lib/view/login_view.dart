6bba60ac2a97c0d85763f98b5be80feadbdd85b2
devansh <devansh2330@users.noreply.github.com>
2022-12-23 00:33:08 +0530

    step-8

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -64,6 +64,18 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    notesRoute,
+                    (route) => false,
+                  );
+                } else {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    verifyEmailRoute,
+                    (route) => false,
+                  );
+                }
                 Navigator.of(context).pushNamedAndRemoveUntil(
                   notesRoute,
                   (route) => false,
